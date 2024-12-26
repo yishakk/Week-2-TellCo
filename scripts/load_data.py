630655656665676a32d928250e4802ea4d19da46
Yishak Kibru <70431682+yishakk@users.noreply.github.com>
2024-12-26 23:13:24 +0300

    import fix

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,3 +1,8 @@
+import os
+from dotenv import load_dotenv
+import pandas as pd
+from sqlalchemy import create_engine
+
 load_dotenv()
 
 # Fetch database connection parameters from environment variables
@@ -7,20 +12,13 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-def load_data_from_postgres(query):
+def load_data_using_sqlalchemy(query):
     try:
+        connection_string = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-        connection = psycopg2.connect(
-            host=DB_HOST,
-            port=DB_PORT,
-            database=DB_NAME,
-            user=DB_USER,
-            password=DB_PASSWORD
-        )
+        engine = create_engine(connection_string)
 
-        df = pd.read_sql_query(query, connection)
-
-        connection.close()
+        df = pd.read_sql_query(query, engine)
 
         return df
 
